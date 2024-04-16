b68ab023f2d545e292832b0b89ca4b1b4e17bc9a
Wisz Gem <165733763+Wiszgem@users.noreply.github.com>
2024-04-16 09:37:40 +0100

    Add files via upload

---
+++ formphp.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <title>Page title</title>
+</head>
+<body>
+     
+     <form method="post" action="https://wiszgem.github.io/Stunning-googles/samplet">
+          
+          <h1>Login</h1>
+          <br>
+          <label for="username">Name</label>
+          <br>
+          <input type="text" id="username" name="username">
+          <br>
+          <br>
+          <label for="password">Password</label>
+          <br>
+          <input type="password" id="password" name="password">
+          <br>
+          <br>
+          <label for="email">Email</label>
+          <br>
+          <input type="email" id="email" name="email">
+          <br>
+          <br>
+          <input type="submit">
+     </form>
+     <img src="https://images.pexels.com/photos/2129796/pexels-photo-2129796.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
+</body>
+</html>
